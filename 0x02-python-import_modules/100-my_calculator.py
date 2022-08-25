d6f839c5b748cf6acfd0ba7d73348ddaa6659038
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-08-25 20:30:17 +0200

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    from sys import argv
+    import sys
 
-    if len(argv) != 4:
+    nargs = len(sys.argv) - 1
+    if nargs != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
-    numb1 = int(argv[1])
-    numb2 = int(argv[3])
-
-    if argv[2] not in ["+", "-", "*", "/"]:
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
-    elif argv[2] is "+":
-        print("{} + {} = {}".format(numb1, numb2, add(numb1, numb2)))
-    elif argv[2] is "-":
-        print("{} - {} = {}".format(numb1, numb2, sub(numb1, numb2)))
-    elif argv[2] is "*":
-        print("{} * {} = {}".format(numb1, numb2, mul(numb1, numb2)))
-    elif argv[2] is "/":
-        print("{} / {} = {}".format(numb1, numb2, div(numb1, numb2)))
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
