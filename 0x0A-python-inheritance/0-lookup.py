5654fa7a2ac55295f4cba8d1d30d804c4539b52d
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-09-26 21:50:22 +0200

    k

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""The ``lookup`` module"""
-
-
-def lookup(obj):
-    """Returns the list of available attributes and methods of an object"""
-    return dir(obj)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""BaseGeometry class module"""
-
-
-class BaseGeometry:
-    """Geomerty utility class"""
-    pass
