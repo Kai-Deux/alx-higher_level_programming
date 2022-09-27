34e60b21d80142199c44a6e0650e7aaf10d632b0
Rohisha Ramauthar <78042229+Kai-Deux@users.noreply.github.com>
2022-09-27 20:21:39 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,49 +1,41 @@
 #!/usr/bin/python3
+
 """
-Script reads stdin line by line and computes metrics
-
-Input format:
-<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-
-Each 10 lines and after a keyboard interruption (CTRL + C),
-prints those statistics since the beginning:
-total file size and
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-
-format: File size: <total size>
-format: <status code (in ascending order)>: <number>
+Module that reads stdin line by line and computes metrics
 """
 
+if __name__ == '__main__':
 
-import sys
+    import sys
 
+    file_size = 0
+    possible_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {i: 0 for i in possible_codes}
+    counter = 0
 
-def print_size_and_codes(size, stat_codes):
-    print("File size: {:d}".format(size))
-    for k, v in sorted(stat_codes.items()):
-        if v:
-            print("{:s}: {:d}".format(k, v))
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(file_size))
+        for i, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(i, v))
 
-
-def parse_stdin_and_compute():
-    size = 0
-    lines = 0
-    stat_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                  "403": 0, "404": 0, "405": 0, "500": 0}
     try:
         for line in sys.stdin:
-            fields = list(map(str, line.strip().split(" ")))
-            size += int(fields[-1])
-            if fields[-2] in stat_codes:
-                stat_codes[fields[-2]] += 1
-            lines += 1
-            if lines % 10 == 0:
-                print_size_and_codes(size, stat_codes)
+            counter += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                file_size += int(data[-1])
+            except BaseException:
+                pass
+            if counter % 10 == 0:
+                print_stats(stats, file_size)
+        print_stats(stats, file_size)
     except KeyboardInterrupt:
-        print_size_and_codes(size, stat_codes)
+        print_stats(stats, file_size)
         raise
-
-    print_size_and_codes(size, stat_codes)
-
-
-parse_stdin_and_compute()
